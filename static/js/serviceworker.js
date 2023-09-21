b70a95a0a882072dfa3d1b1ed3770d4c3681e3da
Katlego909 <67097039+Katlego909@users.noreply.github.com>
2023-09-21 14:53:34 +0200

    first commit

---
+++ static/js/serviceworker.js
@@ -0,0 +1,46 @@
+// sw.js
+
+// Define the cache name and the files to cache
+const CACHE_NAME = 'djangopwa-v1';
+const urlsToCache = [
+  '/',
+  '/static/images/favicon.png',
+  '/static/images/icon-160x160.png',
+  '/static/images/icon.png',
+  // Add more static assets and URLs that you want to cache.
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate the service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch resources from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
